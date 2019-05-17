7827535dc4ece022d4895afdf9b8a111ba104e3c
Sebastián Estrella <sestrella.me@gmail.com>
2019-05-16 21:20:19 -0500

    Extract home-manager configuration to a different file

---
+++ home-manager/sestrella.nix
@@ -0,0 +1,103 @@
+{ pkgs }:
+
+{
+  home.packages = [
+    pkgs.docker
+    pkgs.ffmpeg
+    pkgs.google-chrome
+    pkgs.ripgrep
+    pkgs.slack
+    pkgs.spotify
+    pkgs.vagrant
+  ];
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "GitHub";
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      cat = "bat";
+    };
+    shellAbbrs = {
+      # git
+      ga = "git add";
+      gaa = "git add --all";
+      gbr = "git branch";
+      gc = "git commit -v";
+      gco = "git checkout";
+      gd = "git diff";
+      gl = "git pull";
+      gp = "git push";
+      gst = "git status";
+      # tmux
+      ta = "tmux attach-session -t";
+      tkss = "tmux kill-session -t";
+      tksv = "tmux kill-server";
+      tl = "tmux list-sessions";
+      ts = "tmux new-session -s";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "sestrella.me@gmail.com";
+    userName = "Sebastián Estrella";
+    aliases = {
+      br = "branch";
+      ci = "commit";
+      co = "checkout";
+      st = "status";
+    };
+    extraConfig = {
+      core = {
+        editor = "vim";
+      };
+    };
+  };
+
+  programs.gnome-terminal = {
+    enable = true;
+    showMenubar = false;
+    profile = {
+      "aa6992ba-8e71-468e-abc5-f758989d57b1" = {
+        default = true;
+        visibleName = "sestrella";
+        showScrollbar = false;
+      };
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    baseIndex = 1;
+    terminal = "screen-256color";
+  };
+
+  programs.vim = {
+    enable = true;
+    plugins = [
+      "ctrlp"
+      "nerdtree"
+      "typescript-vim"
+      "vim-nix"
+    ];
+    settings = {
+      expandtab = true;
+      ignorecase = true;
+      number = true;
+      smartcase = true;
+    };
+    extraConfig = ''
+      let g:ctrlp_user_command = '${pkgs.ripgrep}/bin/rg %s --files --color=never --glob ""'
+      let g:ctrlp_use_caching = 0
+
+      noremap <C-n> :NERDTreeToggle<CR>
+    '';
+  };
+}
