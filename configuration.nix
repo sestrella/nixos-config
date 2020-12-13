8d85fcdec9caa1404296266699b036b0b3885c95
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-13 11:59:06 -0500

    Turn on printing and pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -59,11 +59,11 @@
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
