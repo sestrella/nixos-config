562f3a0d5fa24cce86ce4d272d6d92ed5bec7340
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2020-12-14 21:08:03 -0500

    Turn off openssh and postgresql services

--- configuration.nix
+++ configuration.nix
@@ -113,10 +113,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services = {
-    openssh.enable = true;
-    postgresql.enable = true;
-  };
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
